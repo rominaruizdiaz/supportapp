6881547c7386c6ef4554543b131a38ad43699481
Rominaruizdiaz <ruizdiazsanchezromina@gmail.com>
2024-01-05 22:11:19 +0100

    backend created

---
+++ supportapp/src/main/java/dev/rominaruiz/supportapp/SupportappApplication.java
@@ -0,0 +1,13 @@
+package dev.rominaruiz.supportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportappApplication.class, args);
+	}
+
+}
---
+++ supportapp/src/test/java/dev/rominaruiz/supportapp/SupportappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rominaruiz.supportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
